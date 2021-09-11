45fe300465136eb5da4e3a3f97e56c05204dc107
HoangVinhNguyen <ngvinh719@gmail.com>
2021-09-11 17:14:36 +0700

    created project structure

---
+++ SchoolWebParent/SchoolBackEnd/src/main/java/com/school/SchoolBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolBackEndApplication.class, args);
+	}
+
+}
